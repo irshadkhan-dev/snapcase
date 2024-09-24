431f8421641b2af51368841d78d9e2dd180be38c
Irshad khan <irshadkhan.dev@Irshads-MacBook-Air.local>
2024-09-24 20:27:58 +0545

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight, Ghost } from "lucide-react";
+
+const Navbar = () => {
+  const user = undefined;
+  const isAdmin = false;
+  return (
+    <div className="sticky w-full h-14 top-0 z-[100] border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="h-14 border-b border-zinc-200 flex items-center justify-between">
+          <Link href={"/"} className="flex z-40 font-semibold">
+            snap<span className="text-green-600">case</span>
+          </Link>
+
+          <div className="h-full flex items-center space-x-4">
+            {user ? (
+              <>
+                <Link
+                  href={"/api/auth/signout"}
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Sign out
+                </Link>
+
+                {isAdmin ? (
+                  <Link
+                    href={"/api/auth/signout"}
+                    className={buttonVariants({ size: "sm", variant: "ghost" })}
+                  >
+                    Dashboard
+                  </Link>
+                ) : null}
+
+                <Link
+                  href={"/configure/upload"}
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5"></ArrowRight>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href={"/api/auth/signin"}
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Sign up
+                </Link>
+                <Link
+                  href={"/api/auth/signin"}
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Sign in
+                </Link>
+
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+                <Link
+                  href={"/configure/upload"}
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5"></ArrowRight>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/Phone.tsx
@@ -0,0 +1,34 @@
+import React, { HTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  className: string;
+  imgSrc: string;
+  dark?: boolean;
+}
+
+function Phone({ className, imgSrc, dark = false, ...props }: PhoneProps) {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        className="pointer-events-none select-none z-50"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <img src={imgSrc} className="object-cover" />
+      </div>
+    </div>
+  );
+}
+
+export default Phone;
